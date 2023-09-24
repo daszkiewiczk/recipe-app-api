1994be626fa508c743ae2b899ba1fb28a3c36596
Kazimierz Daszkiewicz <daszkiewicz.kazimierz@gmail.com>
2023-09-24 10:23:34 +0200

    user model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -25,3 +25,16 @@ class ModelTests(TestCase):
         for email, expected_email in sample_emails:
             user = get_user_model().objects.create_user(email, 'test123')
             self.assertEqual(user.email, expected_email)
+    
+    def test_new_without_email_raises_error(self):
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test123')
+
+    def test_create_superuser(self):
+        user = get_user_model().objects.create_superuser(
+            'test@example.com',
+            'test123',
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
